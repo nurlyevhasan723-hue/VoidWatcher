f542dbbbd5295eda3f6641b3b5e083063bbe364d
nurlyevhasan723-hue <nurlyevhasan723@gmail.com>
2026-09-20 15:50:52 +0500

    Fix Watcher renderer texture case

--- src/main/java/com/voidwatcher/client/render/WatcherRenderer.java
+++ src/main/java/com/voidwatcher/client/render/WatcherRenderer.java
@@ -2,20 +2,19 @@ package com.voidwatcher.client.render;
 
 import com.voidwatcher.VoidWatcherMod;
 import com.voidwatcher.entity.WatcherEntity;
-
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.util.Identifier;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
 public class WatcherRenderer extends GeoEntityRenderer<WatcherEntity> {
 
-    public WatcherRenderer(EntityRendererFactory.Context ctx) {
-        super(ctx, new WatcherModel());
-        this.shadowRadius = 0.65F;
+    public WatcherRenderer(EntityRendererFactory.Context context) {
+        super(context, new WatcherModel());
+        this.shadowRadius = 0.75F;
     }
 
     @Override
     public Identifier getTextureLocation(WatcherEntity entity) {
-        return Identifier.of(VoidWatcherMod.MOD_ID, "textures/entity/watcher.png");
+        return Identifier.of(VoidWatcherMod.MOD_ID, "textures/entity/Watcher.png");
     }
 }
